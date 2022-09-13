f2add194be9b6eabf551e881d735e86b98469e82
cneal05 <cneal05@charlottesvilleschools.org>
2022-09-13 13:36:08 -0400

    aBGEfjsDFAS

--- raspberry-pi/Launch_Pad_Part_4.py
+++ raspberry-pi/Launch_Pad_Part_4.py
@@ -2,6 +2,9 @@
 import time
 import board
 import digitalio
+import pwmio
+from adafruit_motor import servo
+
 led1 = digitalio.DigitalInOut(board.GP13)
 led1.direction = digitalio.Direction.OUTPUT
 led2 = digitalio.DigitalInOut(board.GP20)
@@ -9,9 +12,12 @@ led2.direction = digitalio.Direction.OUTPUT
 buttonPin = digitalio.DigitalInOut(board.GP28)
 buttonPin.direction = digitalio.Direction.INPUT
 buttonPin.pull = digitalio.Pull.DOWN 
+pwm_servo = pwmio.PWMOut(board.GP2, duty_cycle=2 ** 15, frequency=50)
+servoPin = servo.Servo(pwm_servo, min_pulse=500, max_pulse=2500)
 
 while True:
     if buttonPin.value == True:
+        servoPin.angle = 0
         print(buttonPin.value)
         for counter in range(10, 0, -1):
             print(counter)
@@ -21,4 +27,5 @@ while True:
             time.sleep(0.5)    
         print("Liftoff!")
         led2.value = True
+        servoPin.angle = 180
         time.sleep(3)
