75af80d4a4a81a865e06b16dec23a461c17a63b8
cneal05 <cneal05@charlottesvilleschools.org>
2022-09-16 13:53:34 -0400

    ryfxdl

--- raspberry-pi/code.py
+++ raspberry-pi/code.py
@@ -1,16 +1,21 @@
-
+# type: ignore
 import adafruit_mpu6050
 import busio
 import board
 import time
+import digitalio
 
 sda_pin = board.GP6
 scl_pin = board.GP7
 i2c = busio.I2C(scl_pin, sda_pin)
 mpu = adafruit_mpu6050.MPU6050(i2c)
+led = digitalio.DigitalInOut(board.GP20)
+led.direction = digitalio.Direction.OUTPUT
 
 while True:
     print(mpu.acceleration)
     if mpu.acceleration[0] > 9 or  mpu.acceleration[0] < -9 or mpu.acceleration[1] > 9 or  mpu.acceleration[1] < -9: 
             print("SPIN")
+            led.value = True
+            led.value = False
     time.sleep(1)
