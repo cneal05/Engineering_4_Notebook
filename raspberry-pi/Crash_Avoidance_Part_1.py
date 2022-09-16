069276eb6b7f0d3702a5d83d66ab2985f8d9815f
cneal05 <cneal05@charlottesvilleschools.org>
2022-09-16 13:22:55 -0400

    Crash avoidance part 1

--- raspberry-pi/Crash_Avoidance_Part_1.py
+++ raspberry-pi/Crash_Avoidance_Part_1.py
@@ -1,10 +1,14 @@
-#type: ignore
+# type: ignore
 import adafruit_mpu6050
 import busio
 import board
+import time
 
 sda_pin = board.GP6
 scl_pin = board.GP7
 i2c = busio.I2C(scl_pin, sda_pin)
 mpu = adafruit_mpu6050.MPU6050(i2c)
-print(mpu.acceleration)
+
+while True:
+    print(mpu.acceleration)
+    time.sleep(1)
