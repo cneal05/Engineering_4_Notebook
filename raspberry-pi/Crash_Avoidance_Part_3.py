10e7d46d61bdeeb2336ad973dc27d02af9148b61
cneal05 <cneal05@charlottesvilleschools.org>
2022-09-22 14:08:58 -0400

    adhsd

--- raspberry-pi/Crash_Avoidance_Part_3.py
+++ raspberry-pi/Crash_Avoidance_Part_3.py
@@ -8,13 +8,16 @@ import busio
 import board
 import time
 import digitalio
+displayio.release_displays()
 
 sda_pin = board.GP6
 scl_pin = board.GP7
 i2c = busio.I2C(scl_pin, sda_pin)
-mpu = adafruit_mpu6050.MPU6050(i2c)
+mpu = adafruit_mpu6050.MPU6050(i2c, device_address=0x68 )
 led = digitalio.DigitalInOut(board.GP20)
 led.direction = digitalio.Direction.OUTPUT
+display_bus = displayio.I2CDisplay(i2c, device_address= 0x3d, reset=board.GP15)
+adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
 
 while True:
     print(mpu.acceleration)
