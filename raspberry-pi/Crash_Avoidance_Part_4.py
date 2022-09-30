8d0593d4fe8be5b18bca8b1f39de415f16e407d4
cneal05 <cneal05@charlottesvilleschools.org>
2022-09-30 13:27:15 -0400

    crash4

--- raspberry-pi/Crash_Avoidance_Part_4.py
+++ raspberry-pi/Crash_Avoidance_Part_4.py
@@ -17,14 +17,14 @@ i2c = busio.I2C(scl_pin, sda_pin)
 mpu = adafruit_mpu6050.MPU6050(i2c, address=0x68 )
 led = digitalio.DigitalInOut(board.GP20)
 led.direction = digitalio.Direction.OUTPUT
-sensor = adafruit_mpl3115a2.MPL3115A2(i2c, address = 0x60)
-sensor.sealevel_pressure = 101325
+sensor = adafruit_mpl3115a2.MPL3115A2(i2c, address=0x60)
 display_bus = displayio.I2CDisplay(i2c, device_address= 0x3d, reset=board.GP15)
 display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
- 
+initial = sensor.altitude 
 
 
 while True:
+        new = sensor.altitude - initial
         splash = displayio.Group()
         title = "ANGULAR VELOCITY"
         text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=10)
@@ -37,13 +37,27 @@ while True:
         text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=40)
         splash.append(text_area)   
         display.show(splash)
-        title = "z = %s" % mpu.acceleration[2] 
+        title = "z = %s" % mpu.acceleration[2]  
         text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=55)
         splash.append(text_area)   
         display.show(splash)
-        if sensor.altitude <= 3:
+        
+        #code below was used to check the difference between the initial and new altitude and would display it to the screen.
+        #title = "altitude = %s" % sensor.altitude 
+        #text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=40)
+        #splash.append(text_area)   
+        #display.show(splash)
+        #title = "new = %s" % new 
+        #text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=55)
+        #splash.append(text_area)   
+        #display.show(splash)
+        #title = "initial = %s" % initial 
+        #text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=25)
+        #splash.append(text_area)   
+        #display.show(splash)
+
+        if new <= 3:
             if mpu.acceleration[0] > 9 or  mpu.acceleration[0] < -9 or mpu.acceleration[1] > 9 or  mpu.acceleration[1] < -9: 
-                #print("SPIN")
                 led.value = True
         time.sleep(1)
         led.value = False
