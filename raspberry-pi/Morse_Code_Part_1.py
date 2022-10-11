c32304776b606f53b1d939f13490737b8d55c352
cneal05 <cneal05@charlottesvilleschools.org>
2022-10-11 13:51:17 -0400

    guv

---
+++ raspberry-pi/Morse_Code_Part_1.py
@@ -0,0 +1,32 @@
+#type: ignore
+
+# Dictionary representing the morse code chart
+MORSE_CODE = { 'A':'.-', 'B':'-...',
+    'C':'-.-.', 'D':'-..', 'E':'.',
+    'F':'..-.', 'G':'--.', 'H':'....',
+    'I':'..', 'J':'.---', 'K':'-.-',
+    'L':'.-..', 'M':'--', 'N':'-.',
+    'O':'---', 'P':'.--.', 'Q':'--.-',
+    'R':'.-.', 'S':'...', 'T':'-',
+    'U':'..-', 'V':'...-', 'W':'.--',
+    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+    '1':'.----', '2':'..---', '3':'...--',
+    '4':'....-', '5':'.....', '6':'-....',
+    '7':'--...', '8':'---..', '9':'----.',
+    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+    '?':'..--..', '/':'-..-.', '-':'-....-',
+    '(':'-.--.', ')':'-.--.-', ' ':'/'}
+
+
+while True:
+    print("Enter Morse Code Message, or enter -q to quit:")
+    message = input()
+    if message == "-q":
+        break
+    else:
+        final = ""
+        for letter in message:
+            letter = letter.upper()
+            letter = MORSE_CODE[letter]
+            final = f"{final + letter} "
+        print(final)
